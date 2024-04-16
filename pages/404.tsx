a458daf1b149b288465fc03c32e9254ab9be56bf
Neno Arisma <nenoarisma@gmail.com>
2024-04-16 21:45:41 +0700

    revamp version

--- pages/404.tsx
+++
@@ -1,9 +0,0 @@
-const NothingHere = () => {
-    return (
-        <>
-        <h1 className="font-bold text-2xl flex justify-center m-60">There is nothing here to see | <pre className="text-gray-400 text-2xl"> 404</pre></h1>
-        </>
-    )
-}
-
-export default NothingHere
