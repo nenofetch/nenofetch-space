99bc6bde3d404642706b35c62c9d15e6b912564b
Neno Arisma <nenoarisma@gmail.com>
2024-04-16 21:45:41 +0700

    revamp version

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-xl ">
+      
+        <h1>Coming Soon</h1>
+      
+    </main>
+  );
+}
