48b9de97485348aa7a02597053147f18fe874f6b
Neno Arisma <nenoarisma@gmail.com>
2024-04-24 20:22:24 +0700

    chore: update page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-xl ">
-      
         <h1>Coming Soon</h1>
-      
     </main>
   );
 }
