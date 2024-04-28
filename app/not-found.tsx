af8cab2391fec825a071a5b9c47d4f1d822420e8
Neno Arisma <nenoarisma@gmail.com>
2024-04-28 20:23:04 +0700

    feat(page):adding not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center h-screen">
+  <div className="mt-20">
+    <h1 className="text-4xl font-bold mb-3">404</h1>
+    <h2>Not Found</h2>
+  </div>
+</main>
+
+  );
+}
